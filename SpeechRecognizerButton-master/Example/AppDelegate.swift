827503f214a215bb69787a3856b6be71411bd98c
yousefmankoulah <45926659+yousefmankoulah@users.noreply.github.com>
2021-06-24 22:38:11 -0400

    Add files via upload

---
+++ SpeechRecognizerButton-master/Example/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by yousef mankoulah on 06/24/2021
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
---
+++ SpeechRecognizerButton-master/SpeechRecognizerButton/SpeechRecognizerButton.h
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognizerButton.h
+//  SpeechRecognizerButton
+//
+//  Created by yousef mankoulah on 06/24/2021
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <Speech/Speech.h>
+
+//! Project version number for SpeechRecognizerButton.
+FOUNDATION_EXPORT double SpeechRecognizerButtonVersionNumber;
+
+//! Project version string for SpeechRecognizerButton.
+FOUNDATION_EXPORT const unsigned char SpeechRecognizerButtonVersionString[];
